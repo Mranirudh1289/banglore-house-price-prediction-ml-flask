098fc5beb1b8e8bb0b40c0baf8356fd103a2ade4
Anirudh <mranirudh05@gmail.com>
2025-10-27 17:43:43 +0530

    s

--- server/script.py
+++ server/script.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 from server import util
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/api/get_location_names')
 def get_location_names():
